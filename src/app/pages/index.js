2b657fd47cf7b57943fd57f46083ed9b2f9df718
Jerry Ferrer <jedi@macm2.local>
2023-05-19 16:40:41 -0600

    remove test

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CourseList />
-      <p>test</p>
     </main>
   )
 }
